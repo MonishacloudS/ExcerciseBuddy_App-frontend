6e4cee42f29adde372e3528b6373b209efbcc2b1
MonishacloudS <monishas7122001@gmail.com>
2022-12-21 00:08:51 +0530

    3 commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
+
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
+    const { workouts, dispatch } = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -12,7 +15,7 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok) {
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
 
@@ -26,6 +29,7 @@ const Home = () => {
                     <WorkoutDetails workout={workout} key={workout._id} />
                 ))}
             </div>
+             <WorkoutForm />
         </div>
     )
 }
