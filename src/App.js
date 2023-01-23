b4fe4c6f3995e0b520e53dd09cba5440ebb6e823
MonishacloudS <monishas7122001@gmail.com>
2023-01-23 17:33:20 +0530

    user auth

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
@@ -9,6 +11,7 @@ import Login from "./pages/Login";
 
 
 function App() {
+  const { user } = useAuthContext()
 
   return (
     <div className="App">
@@ -18,15 +21,15 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to="/login" />}
             />
             <Route 
               path="/login" 
-              element={<Login />} 
+              element={!user ? <Login /> : <Navigate to="/" />} 
             />
             <Route 
               path="/signup" 
-              element={<Signup />} 
+              element={!user ? <Signup /> : <Navigate to="/" />} 
             />
           </Routes>
         </div>
--- src/components/WorkoutDetails.js
+++ src/components/WorkoutDetails.js
@@ -1,19 +1,28 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
+import { useAuthContext } from "../hooks/useAuthContext"
 
 // date fns
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({ workout }) => {
     const { dispatch } = useWorkoutsContext()
+    const { user } = useAuthContext()
 
     const handleClick = async () => {
-        const response = await fetch('/api/workouts/' + workout._id, {
-            method: 'DELETE'
+        if (!user) {
+            return
+        }
+
+        const response = await fetch('https://excercise-buddy-app-backend.vercel.app/api/workouts/' + workout._id, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${user.token}`
+            }
         })
         const json = await response.json()
 
-        if (response.ok){
-            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        if (response.ok) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json })
         }
     }
 
@@ -23,7 +32,7 @@ const WorkoutDetails = ({ workout }) => {
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Number of reps: </strong>{workout.reps}</p>
             <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
-      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 }
